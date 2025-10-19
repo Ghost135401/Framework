391efdf5f56ce0ca3f33df886c727c55de10c8e2
RALEVAZAHA Vonjiniaina Emilson <83495723+Ghost135401@users.noreply.github.com>
2025-10-19 15:42:22 +0200

    Create UrlMapping.java

---
+++ src/UrlMapping.java
@@ -0,0 +1,10 @@
+package com.vonjy.spring.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface UrlMapping {
+    String path() default "";
+}
+
